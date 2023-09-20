8c5b4a632016e98617f9e2d01674c6cd245b03d0
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-20 10:39:48 +0400

    test functions

--- functions/test.py
+++ functions/test.py
@@ -1,13 +1,53 @@
 import pandas as pd
-from calculations import Calculations
 
+
+def process_columns_with_colon(df, columns_to_process):
+    """
+    Process specified columns in a DataFrame by replacing values with text after ":".
+
+    Args:
+        df (pd.DataFrame): The DataFrame to process.
+        columns_to_process (list): A list of column names to process.
+
+    Returns:
+        pd.DataFrame: The DataFrame with values in specified columns processed.
+
+    Example:
+        # Create a sample DataFrame
+        data = {'Gender': ['F: Female', 'M: Male', '_T: Total'],
+                'Category': ['A: Category1', 'B: Category2', 'C: Category3']}
+        df = pd.DataFrame(data)
+
+        # Columns to process
+        columns_to_process = ['Gender', 'Category']
+
+        # Call the function to process columns
+        df = process_columns_with_colon(df, columns_to_process)
+
+        # Print the DataFrame with processed columns
+        print(df)
+    """
+    for column in columns_to_process:
+        # Apply a function to each cell in the specified column
+        df[column] = df[column].apply(lambda x: x.split(": ")[1] if ":" in x else x)
+
+    return df
+
+
+# Example usage:
+# Create a sample DataFrame
 data = {
-    "Value": [155, 200, 350, 400, 570],
-    "CO": ["0", "1", "0.2", "0.6", "0"],
-    "CU": ["1", "0", "0.7", "0", "0.6"],
+    "Gender": ["F: Female", "M: Male", "_T: Total"],
+    "Category": ["A: Category1", "B: Category2", "C: Category3"],
+    "Category2": ["Category1_New", "Category2_New", "Category3_New"],
 }
 df = pd.DataFrame(data)
 
-calculation = Calculations(df)
-result = calculation.add_new_columns("Value", "CO", "CU", "new_CO", "new_CU")
-print(result)
+# Columns to process
+columns_to_process = ["Gender", "Category", "Category2"]
+
+# Call the function to process columns
+df = process_columns_with_colon(df, columns_to_process)
+
+# Print the DataFrame with processed columns
+print(df)
