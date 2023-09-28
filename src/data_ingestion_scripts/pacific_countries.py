831fc9212252bac9bc548a36cda220d0115f0d83
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-09-28 19:14:03 +0400

    update script

--- src/data_ingestion_scripts/pacific_countries.py
+++ src/data_ingestion_scripts/pacific_countries.py
@@ -1,44 +1,43 @@
 import pandas as pd
 
-pacific_countries = {
-    "country": [
-        "Vanuatu",
-        "Federated State of Micronesia",
-        "Niue",
-        "New Zealand",
-        "French Polynesia",
-        "Kiribati",
-        "Fiji",
-        "Marshall Islands",
-        "Nauru",
-        "Tonga",
-        "Palau",
-        "Papua New Guinea",
-        "Cook Islands",
-        "New Caledonia",
-        "Wallis and Futuna Islands",
-        "Tokelau",
-        "Australia",
-        "American Samoa",
-        "Solomon Islands",
-        "Tuvalu",
-        "Samoa",
-        "Guam",
-        "Pitcairn",
-        "Northern Mariana Islands",
-    ]
-}
+# Set the maximum number of rows to display (e.g., 100)
+pd.set_option("display.max_rows", 200)
 
-df = pd.DataFrame(pacific_countries)
-df2 = sorted(df["country"])
+# Define the path to the CSV file
+file_path = "../../data/staging/food_trade_imports.csv"
 
-df3 = pd.DataFrame(df2)
+try:
+    # Read the CSV file into a Pandas DataFrame
+    df_raw = pd.read_csv(file_path)
 
-df3.to_csv("../data/processed/pacific_country.csv")
+    # Display the first few rows of the DataFrame to verify data ingestion
+    # print(df_raw.head())
+    df_copy = df_raw.copy()
+    df = pd.DataFrame(df_copy)
+    print(df.head())
 
-df_csv = pd.read_csv("../data/processed/pacific_country.csv")
+    # You can now perform data cleaning, transformation, and exploration on the DataFrame.
+    # For example, you can perform operations like df.describe(), df.isnull().sum(), etc.
 
-df_json = df_csv.to_json()
+
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
+
+df.shape
+
+df2 = df[["Importer", "ImporterISO"]].drop_duplicates().reset_index(drop=True)
+
+df3 = df2.sort_values(by="Importer").reset_index(drop=True)
+
+df3["id"] = range(1, len(df3) + 1)
+
+df3 = df3[["Id", "Importer", "ImporterISO"]]
+
+df4 = df3.rename(columns={"Importer": "country", "ImporterISO": "iso3"})
+
+df4
+
+df_json = df4.to_json("../../data/final/pacific_country.json", orient="records")
 df_json
-
-df_csv.to_json("../data/processed/pacific_country.json", orient="records")
